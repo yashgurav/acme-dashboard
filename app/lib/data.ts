f0df35173a1cdead5980f232d06ac42c1d995c9e
yashgurav <yashgurav4@gmail.com>
2024-03-08 00:30:52 +0530

    added timeout

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -15,6 +15,9 @@ export async function fetchRevenue() {
   noStore();
 
   try {
+    console.log('Fetching revenue data...');
+    await new Promise((resolve) => setTimeout(resolve, 3000));
+
     const data = await sql<Revenue>`SELECT * FROM revenue`;
 
     console.log('Data fetch completed after 3 seconds.');
